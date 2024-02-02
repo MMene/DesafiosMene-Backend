15afd4e113d7e4554c3baeb08ac9689602abf94d
“MMene” <matiasmene@gmail.com>
2024-02-01 23:14:22 -0300

    Desafio 8

---
+++ desafio8/client/public-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    return <footer className="footer">
+        <p>Matias Mene 2024 - Portafolio <a href="https://github.com/MMene">aqui</a></p>
+    </footer>
+}
+
+export default Footer;
